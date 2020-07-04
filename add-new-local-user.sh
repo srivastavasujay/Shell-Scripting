d79780d99a2cf0dcc1cd69cb3c588b49ac42d424
srivastavasujay <sujaysrivastava98@gmail.com>
2020-07-05 02:44:16 +0530

    Added script to create new users and assign random password to them

---
+++ add-new-local-user.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# The goal of this script is to add users to the same Linux system and assign a random one time password
+
+# Make sure the script is being executed with superueser privileges
+if [[ "${UID}" -ne 0 ]]
+then
+	echo "Execute with sudo" 
+	exit 1
+fi
+
+# If the user doesn't supply at least one argument, then give them help
+if [[ "${#}" -lt 1 ]]
+then
+	echo "Usage:${0} USER_NAME [COMMENT]..."
+	echo 'Create an account on the local system with the name of USER_NAME and a comments field of COMMENT.'
+	exit 1
+fi
+
+# The first parameter is the user name
+USER_NAME="${1}"
+
+# The rest of the parameters are for the account comments
+shift
+COMMENT="${@}"
+
+# Generate a passsword
+PASSWORD=$(date +%s%N | sha256sum | head -c48)
+
+# Create user with this password
+useradd -c "${COMMENT}" -m ${USER_NAME} 
+
+#  Check to see if the useradd command succeeded
+if [[ "${?}" -ne 0 ]]
+then
+	echo 'The account could not be created'
+	exit 1
+fi
+
+# Set the password
+echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+
+# Check to see if the passwd command succeeded
+if [[ "${?}" -ne 0 ]]
+then
+	echo 'The password could not be set'
+	exit 1
+fi
+
+# Force password change on first login
+passwd -e ${USER_NAME}
+
+# Display the username, password and hostname
+echo "Username: ${USERNAME}"
+echo "Password: ${PASSWORD}"
+echo "Host: ${HOSTNAME}"
+exit 0
+
